9055dfcbbf72191884fe04e843cd445fffa76274
Santiago A <santiagoaguirreb6@gmail.com>
2025-02-25 10:21:21 -0500

    Respawn Tejo

--- Assets/Scripts/Points.cs
+++ Assets/Scripts/Points.cs
@@ -6,6 +6,37 @@ using UnityEngine.SocialPlatforms.Impl;
 public class Points : MonoBehaviour
 {
     public Score score;
+    public Rigidbody body;
+    public GameObject tejo;
+    public Transform spawnTejo;
+    public float cont;
+    public bool salio = false;
+
+    private void Start()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        body.isKinematic = false;
+
+        if(transform.position.y < -4)
+        {
+            Instantiate(tejo,spawnTejo.position,spawnTejo.rotation);
+            Destroy(gameObject);
+        }
+
+        if (salio == true)
+        {
+            cont = cont + 1 + Time.fixedDeltaTime;
+        }
+        else
+        {
+            cont = 0;
+            salio = false;
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -19,6 +50,18 @@ public class Points : MonoBehaviour
         {
             score.i = score.i + 6;
             Debug.Log("entro bocina");
+        }
+
+        if (other.gameObject.CompareTag("Suelo"))
+        {
+            body.isKinematic = true;
+            salio = true;
+            if(cont >  200)
+            {
+                Instantiate(tejo, spawnTejo.position, spawnTejo.rotation);
+                Destroy(gameObject);
+            }
+            Debug.Log("Entro");
         }
     }
 }
---
+++ Assets/Scripts/SpawnTejo.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnTejo : MonoBehaviour
+{
+    /*public GameObject tejo;
+    public Transform spawnTejo;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Eliminar"))
+        {
+            Instantiate(tejo, spawnTejo.position, spawnTejo.rotation);
+            Destroy(gameObject);
+        }
+    }*/
+}
